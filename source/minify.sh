f23a86d4532ea6d881b7c7fbdd2d0245c36d4f3f
Tony Ivanov <telamohn@gmail.com>
2012-02-20 01:48:46 +0100

    minify.sh: Missed Core.js inclusion, index.htm: Converted to nolonger use php

--- source/minify.sh
+++ source/minify.sh
@@ -1,22 +1,23 @@
 #!/bin/bash
 declare -a sourcess=();
-sources[0]="includes/Unzip.js"
-sources[1]="includes/Amiga.js"
-sources[2]="includes/Soundblaster.js"
-sources[3]="includes/FileLoader.js"
-sources[4]="includes/BPPlayer.js"
-sources[5]="includes/D1Player.js"
-sources[6]="includes/D2Player.js"
-sources[7]="includes/DMPlayer.js"
-sources[8]="includes/FCPlayer.js"
-sources[9]="includes/FXPlayer.js"
-sources[10]="includes/HMPlayer.js"
-sources[11]="includes/MKPlayer.js"
-sources[12]="includes/PTPlayer.js"
-sources[13]="includes/S1Player.js"
-sources[14]="includes/S2Player.js"
-sources[15]="includes/STPlayer.js"
-sources[16]="includes/F2Player.js"
+sources[0]="includes/Core.js"
+sources[1]="includes/Unzip.js"
+sources[2]="includes/Amiga.js"
+sources[3]="includes/Soundblaster.js"
+sources[4]="includes/FileLoader.js"
+sources[5]="includes/BPPlayer.js"
+sources[6]="includes/D1Player.js"
+sources[7]="includes/D2Player.js"
+sources[8]="includes/DMPlayer.js"
+sources[9]="includes/FCPlayer.js"
+sources[10]="includes/FXPlayer.js"
+sources[11]="includes/HMPlayer.js"
+sources[12]="includes/MKPlayer.js"
+sources[13]="includes/PTPlayer.js"
+sources[14]="includes/S1Player.js"
+sources[15]="includes/S2Player.js"
+sources[16]="includes/STPlayer.js"
+sources[17]="includes/F2Player.js"
 
 tmpfile=/tmp/flood-all-raw.js
 
@@ -34,4 +35,4 @@ curl -s \
   -d output_info=compiled_code \
   --data-urlencode "js_code@${tmpfile}" \
   http://closure-compiler.appspot.com/compile \
-  > minified/flodall-min.js
+  > minified/flod-min.js
